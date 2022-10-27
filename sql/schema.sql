93468d5bc6f96e8690d1d828dd7f4ab937758e2b
Matteo-Hamaimi <78216233+Matteo-Hamaimi@users.noreply.github.com>
2022-10-27 02:12:29 +0200

    commit

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,38 +3,20 @@ CREATE DATABASE ChoreCount;
 \c ChoreCount;
 
 -- create a table
-CREATE TABLE users (
-    Id_User Serial PRIMARY KEY,
-    username VARCHAR(50),
-    Firstname VARCHAR(50) NOT NULL,
-    Lastname VARCHAR(50) NOT NULL
+CREATE TABLE Users(
+   username VARCHAR(50),
+   Firstname VARCHAR(50),
+   Lastname VARCHAR(50),
+   Password VARCHAR(50),
+   PRIMARY KEY(username)
 );
 
-CREATE TABLE FlatSharing(
-   Id_FlatSharing INT,
-   FlatSharing_Name VARCHAR(100) NOT NULL,
-   Address VARCHAR(500),
-   City VARCHAR(50),
-   State VARCHAR(50),
-   Zipcode VARCHAR(50),
-   PRIMARY KEY(Id_FlatSharing)
-);
-
-CREATE TABLE Chore_Category(
-   Id_Chore_Category INT,
-   Name_Category VARCHAR(50),
-   Priority VARCHAR(50),
-   Difficulty VARCHAR(50),
-   PRIMARY KEY(Id_Chore_Category)
-);
-
-CREATE TABLE Chore(
-   Id_Chore INT,
-   Chore_Name VARCHAR(100) NOT NULL,
-   Chore_Description VARCHAR(500),
-   Chore_Deadline DATE,
-   Id_Chore_Category INT NOT NULL,
-   PRIMARY KEY(Id_Chore),
-   FOREIGN KEY(Id_Chore_Category) REFERENCES Chore_Category(Id_Chore_Category)
+CREATE TABLE Chores(
+   Id_Chores INT,
+   Chore_Name VARCHAR(250),
+   Description VARCHAR(350),
+   username VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Chores),
+   FOREIGN KEY(username) REFERENCES Users(username)
 );
 
--- sql/seed.sql
+++ sql/seed.sql
@@ -1,10 +1,7 @@
 -- insert value
-INSERT INTO FlatSharing(Id_FlatSharing, FlatSharing_Name, Address, City, State, Zipcode) VALUES ('1', 'Irvine Flatsharing', '66 Peppermint Tree', 'Irvine', 'California', '92618');
+INSERT INTO users(username, Firstname, Lastname, password) VALUES ('emerick_944', 'Emerick', 'Szonyi', '94400');
+INSERT INTO users(username, Firstname, Lastname, password) VALUES ('danny-grn', 'Danny', 'Graine', '94400');
 
-INSERT INTO users(Id_User,username, Firstname, Lastname) VALUES (1,'emerick_944', 'Emerick', 'Szonyi');
-INSERT INTO users(Id_User,username, Firstname, Lastname) VALUES (2,'danny-grn', 'Danny', 'Graine');
-
-INSERT INTO Chore_Category(Id_Chore_Category, Name_Category, Priority, Difficulty) VALUES ('1', 'Kitchen', 'Medium', 'Easy');
-
-INSERT INTO Chore(Id_Chore, Chore_Name, Chore_Description, Chore_Deadline, Id_Chore_Category) VALUES ('4', 'Clean the dishes', 'Clean all the dishes and turn on the dishwasher if necessary', '2022-10-17', '1');
+INSERT INTO Chore(1, 'Clean the dishes', 'Clean the dishes', 'emerick_944');
+INSERT INTO Chore(2, 'Talk about JB', 'Say smt about JB', 'danny-grn');
 
